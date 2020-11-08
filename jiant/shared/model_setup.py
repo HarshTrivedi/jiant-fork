7e60fe6920b0956b93cb55275ce75c8784d34f12
Harsh Trivedi <harshjtrivedi94@gmail.com>
2020-11-08 10:07:40 -0500

    update optimizer.

--- jiant/shared/model_setup.py
+++ jiant/shared/model_setup.py
@@ -67,11 +67,16 @@ class OptimizerSchedulerWithGradOps(OptimizerScheduler):
         self.grad_sim_metric = grad_sim_metric
         self.grad_sim_nonlinear = grad_sim_nonlinear
 
-    def get_shared_grad(self, copy=False):
-        shared_param_grad = [
-            [p.grad for p in g["params"]] if g["shared"] else []
-            for g in self.optimizer.param_groups
-        ]
+    def get_shared_grad(self, copy=False, get_base=True):
+
+        shared_param_grad = [[p.grad for p, is_base in zip(g["params"], g["is_base_encoder"])
+                              if get_base == is_base]  if g["shared"] else []
+                             for g in self.optimizer.param_groups]
+
+        # shared_param_grad = [
+        #     [p.grad for p in g["params"]] if g["shared"] else []
+        #     for g in self.optimizer.param_groups
+        # ]
         if copy:
             shared_param_grad = deepcopy(shared_param_grad)
         return shared_param_grad
@@ -169,6 +174,11 @@ def create_optimizer_from_params(
             ],
             "weight_decay": 0.01,
             "shared": True,
+            "is_base_encoder": [
+                n.startswith("encoder.e")
+                for n, p in used_named_parameters
+                if (n.startswith("encoder.e") or n.startswith("dds_model.encoder.e")) and (not any(nd in n for nd in no_decay))
+            ]
         },
         {
             "params": [
@@ -178,6 +188,7 @@ def create_optimizer_from_params(
             ],
             "weight_decay": 0.005,
             "shared": False,
+            "is_base_encoder": None
         },
         {
             "params": [
@@ -187,6 +198,11 @@ def create_optimizer_from_params(
             ],
             "weight_decay": 0.0,
             "shared": True,
+            "is_base_encoder": [
+                n.startswith("encoder.e")
+                for n, p in used_named_parameters
+                if (n.startswith("encoder.e") or n.startswith("dds_model.encoder.e")) and any(nd in n for nd in no_decay)
+            ]
         },
         {
             "params": [
@@ -196,6 +212,7 @@ def create_optimizer_from_params(
             ],
             "weight_decay": 0.0,
             "shared": False,
+            "is_base_encoder": None
         },
     ]
 
